a45799a8d1a55076aac1fbfc7c604a219c325c25
Darkhax <darklime@live.ca>
2014-12-14 18:52:32 -0700

    Made an ItemBlock, we now have tooltips and creative tab support, all tank variants will also show up in NEI, so that's fun.

---
+++ src/main/java/net/darkhax/gyth/common/items/ItemBlockModularTank.java
@@ -0,0 +1,66 @@
+package net.darkhax.gyth.common.items;
+
+import java.util.List;
+
+import net.darkhax.gyth.Gyth;
+import net.darkhax.gyth.utils.EnumTankData;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemBlockModularTank extends ItemBlock {
+
+    public ItemBlockModularTank(Block block) {
+
+        super(block);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean isAdvanced) {
+
+        if (stack.hasTagCompound()) {
+
+            NBTTagCompound tag = stack.stackTagCompound;
+
+            FluidStack fluid = null;
+
+            if (tag.hasKey("Fluid")) {
+
+                fluid = FluidStack.loadFluidStackFromNBT((NBTTagCompound) tag.getTag("Fluid"));
+                tooltip.add(StatCollector.translateToLocal("tooltip.gyth.fluidName") + ": " + fluid.getLocalizedName());
+            }
+
+            int amount = (fluid != null) ? fluid.amount : 0;
+
+            tooltip.add(StatCollector.translateToLocal("tooltip.gyth.fluidAmount") + ": " + amount + "/" + (tag.getInteger("TankCapacity") * FluidContainerRegistry.BUCKET_VOLUME) + " mB");
+            tooltip.add(StatCollector.translateToLocal("tooltip.gyth.tankTier") + ": " + tag.getString("TierName") + " (" + tag.getInteger("Tier") + ")");
+        }
+
+        else
+            tooltip.add(StatCollector.translateToLocal("tooltip.gyth.itemError"));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs tab, List itemList) {
+
+        for (EnumTankData data : EnumTankData.values()) {
+
+            ItemStack stack = new ItemStack(Item.getItemFromBlock(Gyth.modularTank));
+            NBTTagCompound tag = new NBTTagCompound();
+            tag.setInteger("Tier", data.tier);
+            tag.setString("TierName", data.upgradeName);
+            tag.setInteger("TankCapacity", data.capacity);
+            stack.setTagCompound(tag);
+            itemList.add(stack);
+        }
+    }
+}
