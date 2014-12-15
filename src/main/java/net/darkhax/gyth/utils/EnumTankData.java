71af18b6fda3676985a5ae6ce12b78bb3c35a3e1
Darkhax <darklime@live.ca>
2014-12-14 18:48:33 -0700

    Tank now holds the exact capacity in buckets.

--- src/main/java/net/darkhax/gyth/utils/EnumTankData.java
+++ src/main/java/net/darkhax/gyth/utils/EnumTankData.java
@@ -4,36 +4,37 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public enum EnumTankData {
 
-    ACACIA("acacia", 1),
-    BIRCH("birch", 1),
-    DARKOAK("darkoak", 1),
-    JUNGLE("jungle", 1),
-    OAK("oak", 1),
-    SPRUCE("spruce", 1),
-    STONE("stone", 2),
-    IRON("iron", 3),
-    LAPIS("lapis", 4),
-    REDSTONE("redstone", 4),
-    GOLD("gold", 5),
-    QUARTZ("quartz", 5),
-    DIAMOND("diamond", 6),
-    OBSIDIAN("obsidian", 7),
-    EMERALD("emerald", 8),
-    NETHERSTAR("neterstar", 9);
+    ACACIA("acacia", 1, 4),
+    BIRCH("birch", 1, 4),
+    DARKOAK("darkoak", 1, 4),
+    JUNGLE("jungle", 1, 4),
+    OAK("oak", 1, 4),
+    SPRUCE("spruce", 1, 4),
+    STONE("stone", 2, 16),
+    IRON("iron", 3, 32),
+    LAPIS("lapis", 4, 64),
+    REDSTONE("redstone", 4, 64),
+    GOLD("gold", 5, 128),
+    QUARTZ("quartz", 5, 128),
+    DIAMOND("diamond", 6, 256),
+    OBSIDIAN("obsidian", 7, 512),
+    EMERALD("emerald", 8, 1024);
 
     public String upgradeName;
     public int tier;
+    public int capacity;
 
-    private EnumTankData(String upgradeName, int tier) {
+    private EnumTankData(String upgradeName, int tier, int capacity) {
 
         this.upgradeName = upgradeName;
         this.tier = tier;
+        this.capacity = capacity;
     }
 
-    public static EnumTankData addEnumTankSizeData(String enumName, String upgradeName, int tier) {
+    public static EnumTankData addEnumTankData(String enumName, String upgradeName, int tier, int capacity) {
 
-        Class<?>[] paramTypes = { String.class, int.class };
-        Object[] paramValues = { upgradeName, tier };
+        Class<?>[] paramTypes = { String.class, int.class, int.class };
+        Object[] paramValues = { upgradeName, tier, capacity };
         return EnumHelper.addEnum(EnumTankData.class, enumName, paramTypes, paramValues);
     }
 
