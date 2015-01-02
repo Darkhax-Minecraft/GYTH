bbb15f740e116d1954ae0353f6c4fa07284ebca3
Darkhax <darklime@live.ca>
2015-01-02 03:26:52 -0700

    Changed tab to spruce tank.

--- src/main/java/net/darkhax/gyth/common/tabs/CreativeTabGyth.java
+++ src/main/java/net/darkhax/gyth/common/tabs/CreativeTabGyth.java
@@ -2,9 +2,12 @@ package net.darkhax.gyth.common.tabs;
 
 import java.util.List;
 
+import net.darkhax.gyth.utils.EnumTankData;
+import net.darkhax.gyth.utils.Utilities;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -21,9 +24,17 @@ public class CreativeTabGyth extends CreativeTabs {
         return Items.bucket;
     }
 
+    @Override
     @SideOnly(Side.CLIENT)
     public void displayAllReleventItems(List itemList) {
 
         super.displayAllReleventItems(itemList);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public ItemStack getIconItemStack() {
+
+        return Utilities.getTankStackFromData(EnumTankData.SPRUCE);
     }
 }
