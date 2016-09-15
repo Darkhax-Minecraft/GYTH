4ea21eb7a2b73f9cbfed55771ef641b693264e8f
lclc98 <liam.c14785@gmail.com>
2016-09-15 20:31:09 +1000

    Added item rendering for tanks using bookshelf's new event

--- src/main/java/net/darkhax/gyth/client/ProxyClient.java
+++ src/main/java/net/darkhax/gyth/client/ProxyClient.java
@@ -1,13 +1,13 @@
 package net.darkhax.gyth.client;
 
 import net.darkhax.gyth.Gyth;
+import net.darkhax.gyth.client.renderer.ItemBlockRenderer;
 import net.darkhax.gyth.client.renderer.ModelTank;
 import net.darkhax.gyth.client.renderer.RendererTank;
 import net.darkhax.gyth.common.ProxyCommon;
 import net.darkhax.gyth.tileentity.TileEntityModularTank;
 import net.darkhax.gyth.utils.Constants;
 import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.model.IModel;
@@ -20,37 +20,39 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ProxyClient extends ProxyCommon {
-    
+
     @Override
-    public void registerBlockRenderers () {
-        
+    public void registerBlockRenderers() {
+
         MinecraftForge.EVENT_BUS.register(this);
-        
+
         ModelLoader.setCustomModelResourceLocation(Gyth.itemBlockModularTank, 0, ModelTank.MODEL);
-        ModelLoader.setCustomModelResourceLocation(Gyth.itemTankUpgrade, 0, new ModelResourceLocation(new ResourceLocation("gyth", "tank_upgrade"), null));
-        
+//        ModelLoader.setCustomModelResourceLocation(Gyth.itemTankUpgrade, 0, new ModelResourceLocation(new ResourceLocation("gyth", "tank_upgrade"), null));
+
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityModularTank.class, new RendererTank());
+
+        MinecraftForge.EVENT_BUS.register(new ItemBlockRenderer());
     }
-    
+
     @SubscribeEvent
     public void onModelBake (ModelBakeEvent event) {
-        
+
         try {
-            
+
             final IModel currentModel = ModelLoaderRegistry.getModel(new ResourceLocation("gyth", "block/modular_tank"));
-            
+
             if (currentModel instanceof IRetexturableModel) {
-                
+
                 final IRetexturableModel model = (IRetexturableModel) currentModel;
                 final IBakedModel baseModel = event.getModelRegistry().getObject(ModelTank.MODEL);
-                
+
                 if (baseModel instanceof IPerspectiveAwareModel)
                     event.getModelRegistry().putObject(ModelTank.MODEL, new ModelTank((IPerspectiveAwareModel) baseModel, model));
             }
         }
-        
+
         catch (final Exception exception) {
-            
+
             Constants.LOG.warn(exception);
         }
     }
