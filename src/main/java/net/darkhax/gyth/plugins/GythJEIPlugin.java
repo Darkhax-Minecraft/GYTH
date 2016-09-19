30ce8c816ec964c61e90b28ad208086548053312
Tyler Hancock <darklime@live.ca>
2016-09-19 10:50:28 -0600

    Added support for JEI descriptions.

---
+++ src/main/java/net/darkhax/gyth/plugins/GythJEIPlugin.java
@@ -0,0 +1,19 @@
+package net.darkhax.gyth.plugins;
+
+import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+import net.darkhax.gyth.Gyth;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+@JEIPlugin
+public class GythJEIPlugin extends BlankModPlugin {
+    
+    @Override
+    public void register (IModRegistry registry) {
+        
+        registry.addDescription(new ItemStack(Gyth.itemBlockModularTank, 1, OreDictionary.WILDCARD_VALUE), "jei.gyth.tank.desc");
+        registry.addDescription(new ItemStack(Gyth.itemTankUpgrade, 1, OreDictionary.WILDCARD_VALUE), "jei.gyth.upgrade.desc");
+    }
+}
